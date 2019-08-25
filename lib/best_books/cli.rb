aed01a43213fe0b435287d0415c0ba41c360f99d
Amanda King <amapickell@gmail.com>
2019-08-25 19:31:54 +0000

    added CLI methods

--- lib/best_books/cli.rb
+++ lib/best_books/cli.rb
@@ -1,8 +1,31 @@
 class BestBooks::CLI
+  attr_accessor :year
   
   def call
     #executable method
-    puts "Hello World"
+    @year = Time.now.year - 1
+    list_categories
+    menu
+  end
+  
+  def list_categories
+    #get from scraper on main page
+    puts "Goodreads Choice Awards Categories for #{@year.to_s}"
+    puts "1. Fiction"
+    puts "2. Mystery & Thriller"
+    #@categories = BestBooks::Books.categories
+  end
+  
+  def menu
+    selection = nil
+    while selection != "exit"
+      puts "Choose a category number to see the latest winner or type 'exit'"
+      selection = gets.strip
+      
+      #loop thru categories
+      
+    end
+    
   end
   
 end
