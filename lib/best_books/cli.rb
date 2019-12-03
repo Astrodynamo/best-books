7818082f4b524eda4a88052c9890fb04011adaf4
Amanda King <amapickell@gmail.com>
2019-12-03 16:44:13 +0000

    refactored inside selection method and noted alice's email

--- lib/best_books/cli.rb
+++ lib/best_books/cli.rb
@@ -18,16 +18,19 @@ class BestBooks::CLI
     @books.each.with_index(1) do |book, i| 
       puts "#{i}. #{book.category}"
     end
-  end
+  end #alice@flatironschool.com
   
   def menu
     selection = nil
+    max = @books.size
+    
     while selection != "exit"
       puts "Choose a category number to see the latest winner, type 'list' to see the list again, or type 'exit'."
       selection = gets.strip.downcase
+      selection_number = selection.to_i
       
-      if selection.to_i > 0 && selection.to_i <= @books.size
-        book = @books[selection.to_i - 1] ### Can combine line 30 + 31?
+      if selection_number > 0 && selection_number <= max
+        book = @books[selection_number - 1]
         book.add_deets_if_needed
         puts "\nBest #{book.category} #{@year}:" ### This is weird for #5 'best of the best' category - add a nested if statement to account for category starting with "Best"
         puts "#{book.title}"
